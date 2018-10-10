b475bb84a1623ae91910fb10853ea2faee011998
Trey Dockendorf <treydock@gmail.com>
2018-10-10 18:44:08 -0400

    Convert boolean values to strings and back to work around issues with true/false merging handling with deep_merge

--- lib/rzo/app/generate.rb
+++ lib/rzo/app/generate.rb
@@ -33,19 +33,27 @@ module Rzo
       #
       # @return [Array<Hash>] list of agent nodes to fill in the Vagrantfile
       # template.
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def vagrantfile_agents(config)
         pm_settings = puppetmaster_settings(config)
         agent_nodes = [*config['nodes']].reject do |n|
           pm_settings['name'].include?(n['name'])
         end
 
-        agent_nodes.each do |n|
-          n.deep_merge(config['defaults'])
+        agent_nodes.map! do |n|
+          new_n = deep_convert_boolean(n)
+          defaults = deep_convert_boolean(config['defaults'])
+          new_n.deep_merge(defaults)
+          n = deep_unconvert_boolean(new_n)
           log.debug "puppetagent #{n['name']} = \n" + n.pretty_inspect
+          n
         end
 
         agent_nodes
       end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/AbcSize
 
       ##
       # Return a list of puppetmaster node definitions suitable for the
@@ -58,19 +66,26 @@ module Rzo
       # Vagrantfile template.
       #
       # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def vagrantfile_puppet_masters(config)
         pm_settings = puppetmaster_settings(config)
         pm_names = pm_settings['name']
 
         nodes = [*config['nodes']].find_all { |n| pm_names.include?(n['name']) }
-        nodes.each do |n|
-          n.deep_merge(config['defaults'])
-          n.deep_merge(pm_settings)
+        nodes.map! do |n|
+          new_n = deep_convert_boolean(n)
+          defaults = deep_convert_boolean(config['defaults'])
+          new_pm_settings = deep_convert_boolean(pm_settings)
+          new_n.deep_merge(defaults)
+          new_n.deep_merge(new_pm_settings)
+          n = deep_unconvert_boolean(new_n)
           n[:puppetmaster] = true
           log.debug "puppetmaster #{n['name']} = \n" + n.pretty_inspect
+          n
         end
       end
       # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
 
       ##
       # Return the proxy configuration exception list as a string, or nil if not set.
@@ -133,6 +148,46 @@ module Rzo
 
       # Constants used by the Vagrantfile.erb template.
       DEFAULT_NO_PROXY = 'localhost,127.0.0.1'.freeze
+
+      private
+
+      ##
+      # Convert boolean to string for deep_merge
+      # rubocop:disable Metrics/MethodLength
+      def deep_convert_boolean(hash)
+        new_hash = {}
+        hash.each_pair do |key, value|
+          if value.is_a?(Hash)
+            new_hash[key] = deep_convert_boolean(value)
+          elsif value.is_a?(TrueClass)
+            new_hash[key] = 'true'
+          elsif value.is_a?(FalseClass)
+            new_hash[key] = 'false'
+          else
+            new_hash[key] = value
+          end
+        end
+        return new_hash
+      end
+
+      ##
+      # Convert boolean strings to boolean for use after deep_merge
+      def deep_unconvert_boolean(hash)
+        new_hash = {}
+        hash.each_pair do |key, value|
+          if value.is_a?(Hash)
+            new_hash[key] = deep_unconvert_boolean(value)
+          elsif value == 'true'
+            new_hash[key] = true
+          elsif value == 'false'
+            new_hash[key] = false
+          else
+            new_hash[key] = value
+          end
+        end
+        return new_hash
+      end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
